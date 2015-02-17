2ad226cd6eb918f99eb83062954fcc06199cb24f
Hugh <hugh_77@icloud.com>
2015-02-17 20:03:28 +0800

    modificate ui of mainform

--- src/ui/MainForm.java
+++ src/ui/MainForm.java
@@ -10,6 +10,31 @@ public class MainForm {
     private JPanel controlPanel;
     private JLabel timeLabel;
     private JSlider slider1;
+    private JPanel viewChoicePanel;
+    private JButton 行车轨迹Button;
+    private JButton velocityButton;
+    private JButton laneButton;
+    private JButton accelerationButton;
+    private JButton evaluationButton;
+    private JPanel timeControlPanel;
+    private JPanel viewPanel;
+    private JButton dashboradButton;
+    private JPanel exporDataPanel;
+    private JButton setStartPointButton;
+    private JButton setEndPointButton;
+    private JButton exportDataButton;
+    private JTextField filePathTextField;
+    private JButton filePathChooseButton;
+    private JButton startAndPauseButton;
+    private JButton playSpeed1;
+    private JButton playSpeed2;
+    private JButton playSpeed3;
+    private JButton playSpeed4;
+    private JButton playSpeed5;
+    private JButton playSpeed6;
+    private JButton playSpeed7;
+    private JButton playSpeed8;
+    private JLabel filePathLabel;
 
     public static void main(String[] args) {
         try {
