895a3edb7e8a498abd4905fc15a1a54ec49521de
AllenLyu <allen.walker.lc@gmail.com>
2015-02-23 23:05:14 +0800

    del some comflict

--- src/ui/Lane.java
+++ src/ui/Lane.java
@@ -6,13 +6,9 @@ import javax.swing.*;
  * Created by Hugh on 2015/2/16 0016.
  */
 public class Lane {
-<<<<<<< HEAD
-
-=======
     private JPanel lanePanel;
 
     public JPanel getLanePanel() {
         return lanePanel;
     }
->>>>>>> dev
 }
