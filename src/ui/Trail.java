21f109ff32e2228b0c0bcb3d5eaad64740d93786
AllenLyu <allen.walker.lc@gmail.com>
2015-02-26 11:48:55 +0800

    please ignore this commit-------by allen

--- src/ui/Trail.java
+++ src/ui/Trail.java
@@ -1,5 +1,16 @@
 package ui;
 
+import data.structure.SimulatedVehicle;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
 import javax.swing.*;
 
 /**
